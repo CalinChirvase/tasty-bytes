e470655da858cadeb0b8cb2dd0cfb69806d865bb
CalinChirvase <calin.chirvase@yahoo.com>
2021-02-16 14:37:07 -0500

    add login/logout functionality

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,34 +1,49 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import { Container } from '@material-ui/core'
 import { Switch, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
-import Image from './assets/test-image.jpg'
-import { withStyles } from '@material-ui/core/styles'
+//import Image from './assets/test-image.jpg'
+import { ThemeProvider } from '@material-ui/styles'
+import theme from './components/theme'
+//import { withStyles } from '@material-ui/core/styles'
+import blogService from './services/blogs'
 
-const styles = {
-  '@global': {
-    body: {
-      backgroundImage: `url(${Image})`,
-      backgroundSize: 'cover'
-    }
-  }
-}
+//const styles = {
+//  '@global': {
+//    body: {
+//      backgroundImage: `url(${Image})`,
+//      backgroundSize: 'cover'
+//    }
+//  }
+//}
 const App = () => {
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedTastyBytesUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   return (
-    <Container>
-      <NavBar />
-      <Switch>
-        <Route path='/login'>
-          <LoginForm />
-        </Route>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <NavBar />
+        <Switch>
+          <Route path='/login'>
+            <LoginForm />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Container>
+    </ThemeProvider>
   )
 }
 
-export default withStyles(styles)(App)
+//export default withStyles(styles)(App)
+export default App
--- react-ui/src/components/NavBar.js
+++ react-ui/src/components/NavBar.js
@@ -1,36 +1,61 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
-//import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
-//import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import { makeStyles } from '@material-ui/styles'
+import logo from '../assets/logo.png'
+import { logout } from '../reducers/loginReducer'
+import { useDispatch, useSelector } from 'react-redux'
 
-//const useStyles = makeStyles((theme) => ({
-//  root: {
-//    flexGrow: 1,
-//  },
-//  menuButton: {
-//    marginRight: theme.spacing(2),
-//  },
-//  title: {
-//    flexGrow: 1,
-//  },
-//}))
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: '3em'
+  },
+  logo: {
+    height: '7em'
+  }
+}))
+
 
 const NavBar = () => {
-  //const classes = useStyles()
+
+  const dispatch = useDispatch()
+  const classes = useStyles()
+  const user = useSelector(state => state.user)
+
+  const handleLogout = () => {
+    dispatch(logout())
+  }
 
   return (
-    <AppBar position="fixed" color="primary">
-      <Toolbar>
-        <Button color="inherit" component={Link} to="/">
-          <Typography variant="h6" color="inherit">
-              Tasty Bytes
-          </Typography>
-        </Button>
-        <Button color="inherit" component={Link} to="/login">Login</Button>
-      </Toolbar>
-    </AppBar>
+    <React.Fragment>
+      <AppBar position="fixed" color="primary">
+        <Toolbar>
+          <img src={logo} alt="website logo" className={classes.logo} />
+          <Button color="inherit" component={Link} to="/">
+            <Typography variant="h6" color="inherit">
+                Home
+            </Typography>
+          </Button>
+          {user.token === undefined
+            ?
+            <Button color="inherit" component={Link} to="/login">
+              <Typography variant="h6" color="inherit">
+                Login
+              </Typography>
+            </Button>
+            :
+            <Button onClick={() => handleLogout()} color="inherit" component={Link} to="/">
+              <Typography variant="h6" color="inherit">
+                Logout
+              </Typography>
+            </Button>
+          }
+        </Toolbar>
+      </AppBar>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
   )
 }
 export default NavBar
