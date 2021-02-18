cc5ab256f49bcc4401431bd48bcc289a8281cc79
CalinChirvase <calin.chirvase@yahoo.com>
2021-02-18 14:05:45 -0500

    change NavBar buttons to tabs instead

--- react-ui/src/components/NavBar.js
+++ react-ui/src/components/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
-import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
-import { makeStyles } from '@material-ui/styles'
 import logo from '../assets/logo.png'
 import { logout } from '../reducers/loginReducer'
 import { useDispatch, useSelector } from 'react-redux'
+
+import { makeStyles } from '@material-ui/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 
 const useStyles = makeStyles(theme => ({
   toolbarMargin: {
@@ -14,6 +17,14 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     height: '7em'
+  },
+  tabContainer: {
+    marginLeft: 'auto'
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px'
   }
 }))
 
@@ -34,16 +45,10 @@ const NavBar = () => {
         <AppBar position="fixed" color="primary">
           <Toolbar>
             <img src={logo} alt="website logo" className={classes.logo} />
-            <Button color="inherit" component={Link} to="/">
-              <Typography variant="h6" color="inherit">
-                  Home
-              </Typography>
-            </Button>
-            <Button color="inherit" component={Link} to="/login">
-              <Typography variant="h6" color="inherit">
-                Login
-              </Typography>
-            </Button>
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" component={Link} to="/" />
+              <Tab className={classes.tab} label="Login" component={Link} to="/login" />
+            </Tabs>
           </Toolbar>
         </AppBar>
         <div className={classes.toolbarMargin} />
@@ -56,21 +61,11 @@ const NavBar = () => {
       <AppBar position="fixed" color="primary">
         <Toolbar>
           <img src={logo} alt="website logo" className={classes.logo} />
-          <Button color="inherit" component={Link} to="/">
-            <Typography variant="h6" color="inherit">
-                Home
-            </Typography>
-          </Button>
-          <Button color="inherit" component={Link} to="/blogs">
-            <Typography variant="h6" color="inherit">
-                Blogs
-            </Typography>
-          </Button>
-          <Button onClick={() => handleLogout()} color="inherit" component={Link} to="/">
-            <Typography variant="h6" color="inherit">
-              Logout
-            </Typography>
-          </Button>
+          <Tabs className={classes.tabContainer}>
+            <Tab className={classes.tab} label="Home" component={Link} to="/" />
+            <Tab className={classes.tab} label="Blogs" component={Link} to="/blogs" />
+            <Tab onClick={() => handleLogout()} className={classes.tab} label="Logout" component={Link} to="/" />
+          </Tabs>
         </Toolbar>
       </AppBar>
       <div className={classes.toolbarMargin} />
--- react-ui/src/components/theme.js
+++ react-ui/src/components/theme.js
@@ -18,6 +18,12 @@ const theme = createMuiTheme({
   typography: {
     h3: {
       fontWeight: 300
+    },
+    tab: {
+      fontFamily: 'Raleway',
+      textTransform: 'none',
+      fontWeight: 700,
+      fontSize: '1.4rem',
     }
   },
   status: {
