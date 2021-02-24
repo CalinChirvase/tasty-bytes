3be40885f6fd9bb0f5f08fabe55d59cda11c41f1
CalinChirvase <calin.chirvase@yahoo.com>
2021-02-24 15:34:48 -0500

    add missing path to serve static files

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const loginRouter = require('./controllers/login')
+const path = require('path')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -21,7 +22,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     })
 
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')))
 app.use(express.json())
 
 app.use(middleware.requestLogger)
